d0ae9e4398486b8099bf9841c0c007e82816f410
Óscar Bravo Sarro <oscar.bravo@innocv.com>
2019-10-11 17:21:05 +0200

    feature/layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public logList = [
+    {
+      name: 'Reunión 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas tincidunt massa id placerat.',
+      date: '01/02/2019',
+      image: '/assets/shapes.svg',
+    },
+    {
+      name: 'Reunión 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas tincidunt massa id placerat.',
+      date: '01/02/2019',
+      image: '/assets/shapes.svg',
+    },
+    {
+      name: 'Reunión 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas tincidunt massa id placerat.',
+      date: '01/02/2019',
+      image: '/assets/shapes.svg',
+    },
+    {
+      name: 'Reunión 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas tincidunt massa id placerat.',
+      date: '01/02/2019',
+      image: '/assets/shapes.svg',
+    }
+  ];
+
   constructor() {}
 
 }
