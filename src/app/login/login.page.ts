10a45ebe21c5bb2eecec332711cc2682c3724d87
Sergio Juntas <sergio.juntas@innocv.com>
2019-10-11 18:25:09 +0200

    http

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss']
 })
+// tslint:disable-next-line:component-class-suffix
 export class LoginPage implements OnInit {
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/login', JSON.stringify(item)]);
-  // }
+
+  auth(item: any) {
+    this.router.navigate(['/home']);
+  }
 }
